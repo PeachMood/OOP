fedbb5125516c7ebbe4bd3a44e7757bc02199670
PeachMood <vas140302@gmail.com>
2021-10-01 15:16:14 +0200

    Working solution for big files, but broken testing system

---
+++ Task_1_2/src/main/java/ru/nsu/voronova/SearchSubstring.java
@@ -0,0 +1,108 @@
+package ru.nsu.voronova;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SearchSubstring {
+  public static boolean isEmptyString(BufferedReader reader) throws IOException {
+    if (reader.markSupported()) {
+      reader.mark(1);
+    }
+
+    if (reader.read() == -1) {
+      reader.reset();
+      return true;
+    }
+    return false;
+  }
+
+  public static int getStringLength(BufferedReader reader, int maxStringLength) throws IOException {
+    if (reader.markSupported()) {
+      reader.mark(maxStringLength);
+    }
+    int length = 0;
+    while (reader.read() != -1) length++;
+    reader.reset();
+    return length;
+  }
+
+  private static ArrayList<Integer> zFunction(BufferedReader pattern, BufferedReader text) throws IOException {
+    if (isEmptyString(pattern)) return null;
+
+    if (isEmptyString(text)) return null;
+
+    final int maxStringLength = 2048;
+    final int patternLength = getStringLength(pattern, maxStringLength);
+    char[] patternBuffer = new char[patternLength];
+
+    int readCount = pattern.read(patternBuffer, 0, patternLength);
+
+    int bufferSize = 6 * patternLength;
+    char[] buffer = Arrays.copyOf(patternBuffer, bufferSize);
+
+    int shift = 0;
+    int left = 0;
+    int right = 0;
+    int[] z = new int[patternLength];
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int i = 1; readCount != -1; ++i) {
+      int currentZ = (right > i) ? Math.min(z[i - left], right - i) : 0;
+
+      int index = i - shift;
+      if (index == bufferSize || i == patternLength) {
+        shift += readCount;
+        index -= readCount;
+        readCount = text.read(buffer, 0, bufferSize);
+        bufferSize = readCount;
+        if (readCount != -1) {
+          buffer = Arrays.copyOfRange(buffer, 0, readCount);
+        }
+      }
+
+      while (currentZ < patternLength && index + currentZ < bufferSize && patternBuffer[currentZ] == buffer[index + currentZ]) {
+        currentZ++;
+        if (i >= patternLength && index + currentZ == bufferSize) {
+          int afterIndex = bufferSize - index;
+          buffer = Arrays.copyOfRange(buffer, index, bufferSize + index);
+          shift += readCount;
+          readCount = text.read(buffer, afterIndex, index);
+          bufferSize = afterIndex;
+
+          if (readCount != -1) {
+            bufferSize += readCount;
+          }
+
+          shift -= afterIndex;
+          index = i - shift;
+        }
+      }
+
+      if (i < patternLength) {
+        z[index] = currentZ;
+      }
+
+      if (i + currentZ > right) {
+        left = i;
+        right = i + currentZ;
+      }
+
+      if (i >= patternLength && currentZ == patternLength) {
+        result.add(i - patternLength);
+      }
+    }
+    return result;
+  }
+
+  public static ArrayList<Integer> search(Reader patternReader, Reader textReader) throws IOException {
+    BufferedReader pattern = new BufferedReader(patternReader);
+    BufferedReader text = new BufferedReader(textReader);
+
+    ArrayList<Integer> result = zFunction(pattern, text);
+
+    pattern.close();
+    text.close();
+    return result;
+  }
+}
+
---
+++ Task_1_2/src/test/java/ru/nsu/voronova/SearchSubstringTest.java
@@ -0,0 +1,83 @@
+package ru.nsu.voronova;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchSubstringTest {
+
+  private static int getStringLength(Reader reader, int maxStringLength) throws IOException {
+    if (reader.markSupported()) {
+      reader.mark(maxStringLength);
+    }
+    int length = 0;
+    while (reader.read() != -1) length++;
+    reader.reset();
+    return length;
+  }
+
+  private String readerToString(Reader reader) throws IOException {
+    final int patternLength = getStringLength(reader, 2048);
+    char[] buffer = new char[patternLength];
+    reader.read(buffer, 0, patternLength);
+    return String.valueOf(buffer);
+  }
+
+  private ArrayList<Integer> referenceSearch(Reader patternReader, Reader textReader) throws IOException {
+    String pattern = readerToString(patternReader);
+    final int patternLength = pattern.length();
+
+    BufferedReader text = new BufferedReader(textReader);
+    int bufferSize = 6 * patternLength;
+    char[] buffer = new char[bufferSize];
+
+    ArrayList<Integer> result = new ArrayList<>();
+
+    int index;
+    int shift = 0;
+    int readCount = text.read(buffer, 0,  bufferSize);
+    while ( readCount != -1) {
+      String temp = String.valueOf(buffer);
+      index = temp.indexOf(pattern);
+      result.add(index - shift);
+      int afterIndex = bufferSize - index;
+      buffer = Arrays.copyOfRange(buffer, index, bufferSize + index);
+      shift += readCount;
+      readCount = text.read(buffer, afterIndex, index);
+      bufferSize = afterIndex;
+    }
+
+    return result;
+  }
+
+  @Test
+  public void testSearch_emptyPattern() throws IOException {
+    String patternString = "";
+    String textString = "Have a good day!";
+    ArrayList<Integer> result = SearchSubstring.search(new StringReader(patternString), new StringReader(textString));
+    assertNull(result);
+  }
+
+  @Test
+  public void testSearch_emptyString() throws IOException {
+    String patternString = "Wish you all the best!";
+    String textString = "";
+    ArrayList<Integer> result = SearchSubstring.search(new StringReader(patternString), new StringReader(textString));
+    assertNull(result);
+  }
+
+  @Test
+  public void testSearch_smallString() throws IOException {
+    String patternString = "빺";
+    String textString = "滌빺呺뚖똼埉뤌빺ﺤ丕䓂鿦빺쉦艪빺";
+    ArrayList<Integer> result = SearchSubstring.search(new StringReader(patternString), new StringReader(textString));
+    ArrayList<Integer> expected = referenceSearch(new StringReader(patternString), new StringReader(textString));
+    Assertions.assertEquals(result, expected);
+  }
+}

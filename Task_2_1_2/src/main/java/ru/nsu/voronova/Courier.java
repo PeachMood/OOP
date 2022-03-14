344c07f2ef288b0db501d98ed505fca058605e81
a.voronova <a.voronova1@g.nsu.ru>
2022-03-14 13:58:54 +0700

    Some fixes

--- Task_2_1_2/src/main/java/ru/nsu/voronova/Courier.java
+++ Task_2_1_2/src/main/java/ru/nsu/voronova/Courier.java
@@ -1,4 +1,22 @@
 package ru.nsu.voronova;
 
-public class Courier extends Consumer<Order> {
+import java.util.List;
+
+public class Courier extends Consumer {
+    private final int bagCapacity;
+    private final QueueBlocking<Order> storage;
+    private final long MAX_DELIVERY_TIME = 100;
+
+    public Courier(int bagCapacity, QueueBlocking<Order> storage) {
+        this.bagCapacity = bagCapacity;
+        this.storage = storage;
+    }
+
+    @Override
+    public void consume() throws InterruptedException {
+        List<Order> orders = storage.get(bagCapacity);
+        for (Order order : orders) {
+            Thread.sleep(MAX_DELIVERY_TIME);
+        }
+    }
 }
--- Task_2_1_2/src/main/java/ru/nsu/voronova/Order.java
+++ Task_2_1_2/src/main/java/ru/nsu/voronova/Order.java
@@ -6,4 +6,19 @@ public class Order {
     public Order(int number) {
         this.number = number;
     }
+
+
+    private enum State {
+        IN_QUEUE("is in the queue"), COOKING("is being cooked"), IN_STOCK("is in the storage"),
+        DELIVERING("is being delivered by courier"), DELIVERED("was delivered");
+        private final String state;
+
+        State(String state) {
+            this.state = state;
+        }
+
+        public String getState() {
+            return state;
+        }
+    }
 }

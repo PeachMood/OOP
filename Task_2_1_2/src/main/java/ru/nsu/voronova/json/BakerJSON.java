d5a8cfca2b83005887aa6e54b0acfe70a82bb76c
a.voronova <a.voronova1@g.nsu.ru>
2022-03-27 17:03:37 +0700

    Some forgotten files

---
+++ Task_2_1_2/src/main/java/ru/nsu/voronova/json/BakerJSON.java
@@ -0,0 +1,14 @@
+package ru.nsu.voronova.json;
+
+public class BakerJSON {
+    private int id;
+    private int workingExperience;
+
+    public int id() {
+        return id;
+    }
+
+    public int workingExperience() {
+        return workingExperience;
+    }
+}
---
+++ Task_2_1_2/src/main/java/ru/nsu/voronova/json/CourierJSON.java
@@ -0,0 +1,14 @@
+package ru.nsu.voronova.json;
+
+public class CourierJSON {
+    private int id;
+    private int bagCapacity;
+
+    public int id() {
+        return id;
+    }
+
+    public int bagCapacity() {
+        return bagCapacity;
+    }
+}

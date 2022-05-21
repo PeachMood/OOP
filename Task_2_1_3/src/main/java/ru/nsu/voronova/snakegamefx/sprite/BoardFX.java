74a944b637c29842a980efaeee323e3799c6d68c
a.voronova <a.voronova1@g.nsu.ru>
2022-05-22 03:28:49 +0700

    Completed javadoc

--- Task_2_1_3/src/main/java/ru/nsu/voronova/snakegamefx/sprite/BoardFX.java
+++ Task_2_1_3/src/main/java/ru/nsu/voronova/snakegamefx/sprite/BoardFX.java
@@ -6,13 +6,28 @@ import ru.nsu.voronova.snakegamefx.skin.Skin;
 import ru.nsu.voronova.snakegame.cell.Cell;
 import ru.nsu.voronova.snakegame.sprite.board.Board;
 
+/**
+ * Implementation of the abstract class Board.
+ * Implements the {@link #render(Object)} method for the board image on the specified frame.
+ */
 public class BoardFX extends Board {
     private Skin skin;
 
+    /**
+     * Class constructor. Creates a board with specified width and height.
+     *
+     * @param width  - board width.
+     * @param height - board height.
+     */
     public BoardFX(double width, double height) {
         super(width, height);
     }
 
+    /**
+     * Changes board skin used for its rendering.
+     *
+     * @param skin - new board skin.
+     */
     public void setSkin(Skin skin) {
         this.skin = skin;
     }
@@ -23,6 +38,11 @@ public class BoardFX extends Board {
         return imageView;
     }
 
+    /**
+     * Renders board on the specified frame.
+     *
+     * @param object - object on which board should be rendered.
+     */
     @Override
     public void render(Object object) {
         Group frame = ((Group) object);
--- Task_2_1_3/src/main/java/ru/nsu/voronova/snakegamefx/sprite/FruitFX.java
+++ Task_2_1_3/src/main/java/ru/nsu/voronova/snakegamefx/sprite/FruitFX.java
@@ -6,13 +6,28 @@ import ru.nsu.voronova.snakegame.cell.Cell;
 import ru.nsu.voronova.snakegame.sprite.fruit.Fruit;
 import ru.nsu.voronova.snakegamefx.skin.Skin;
 
+/**
+ * Implementation of the abstract class Fruit, which provides an interface
+ * for rendering a fruit on the specified frame depending on fruit coordinates and bounds.
+ */
 public class FruitFX extends Fruit {
     private Skin skin;
 
+    /**
+     * Class constructor. Creates a fruit with specified width and height.
+     *
+     * @param width  - fruit width.
+     * @param height - fruit height.
+     */
     public FruitFX(double width, double height) {
         super(width, height);
     }
 
+    /**
+     * Changes fruit skin used for its rendering.
+     *
+     * @param skin - fruit skin.
+     */
     public void setSkin(Skin skin) {
         this.skin = skin;
     }
@@ -23,6 +38,11 @@ public class FruitFX extends Fruit {
         return imageView;
     }
 
+    /**
+     * Renders fruit on the specified frame depending on its coordinates and bounds.
+     *
+     * @param object - object on which fruit should be rendered.
+     */
     @Override
     public void render(Object object) {
         Group frame = ((Group) object);
